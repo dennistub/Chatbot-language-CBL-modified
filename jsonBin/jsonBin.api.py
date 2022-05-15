879c7a0bd9a255ffc510048ccd1d136d82781902
dennisko762 <dennis.korolevych@campus.tu-berlin.de>
2022-05-15 22:20:33 +0200

    update

--- jsonBin/jsonBin.api.py
+++ jsonBin/jsonBin.api.py
@@ -34,73 +34,88 @@ import aiohttp
 
 from io import StringIO
 binsArray = []
+
+
 async def getBins():
-    
-    url = 'https://api.jsonbin.io/v3/c/uncategorized/bins' 
+
+    url = 'https://api.jsonbin.io/v3/c/uncategorized/bins/627aa31b38be296761005d67'
     headers = {
-    'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
+        'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
     }
     data = {}
     req = requests.get(url, json=data, headers=headers)
     io = StringIO(req.text)
-    bins = json.load(io);
+    bins = json.load(io)
     binHeaders = {
-    'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
+        'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
     }
 
     for _bin in bins:
-            binUrl = 'https://api.jsonbin.io/v3/b/{}'.format(_bin["record"])
-            req = requests.get(binUrl, json=binsArray, headers=binHeaders)
-            bin = req.json()
-            binsArray.append(bin);
-                # binData = {}
-                # binReq = await requests.get(binUrl, json=binData, headers=binHeaders).json();
-    
-    
-    # url = 'http://jsonplaceholder.typicode.com/users'
-    # data = {}
-    # req = requests.get(url, json=data);
-    
-    # for _user in req:
-    #     #with open('results.json', 'w') as f:
-    #     bins.append(json.load(StringIO(_user)))
-    #     #print(bins)
+        binUrl = 'https://api.jsonbin.io/v3/b/{}'.format(_bin["record"])
+        req = requests.get(binUrl, json=binsArray, headers=binHeaders)
+        bin = req.json()
+        binsArray.append(bin)
+# binData = {}
+# binReq = await requests.get(binUrl, json=binData, headers=binHeaders).json();
+
+# url = 'http://jsonplaceholder.typicode.com/users'
+# data = {}
+# req = requests.get(url, json=data);
+
+# for _user in req:
+#     #with open('results.json', 'w') as f:
+#     bins.append(json.load(StringIO(_user)))
+#     #print(bins)
+
+
 async def main():
     await asyncio.gather(getBins())
     print("I have waited for it")
-    with open('results.json', 'w') as f:
-        json.dump(binsArray, f)
+    with open('results.json', 'rb') as f:
+        if f.read(0) == '':
+            f.seek(0)
+            with open('results.json', 'w') as file:
+                json.dump(binsArray, file)
+        else:
+            with open('results.json') as json_file:
+                print("its not empty")
+                data = json.load(json_file)
+                # print(data)
+                data.append(
+                    binsArray)
+            with open('results.json', 'w') as bla:
+                json.dump(data, bla, indent=4)
+
 
 asyncio.run(main())
-    
-    
-    # url = 'https://api.jsonbin.io/v3/c/uncategorized/bins' 
-    # headers = {
-    # 'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
-    # }
-    # data = {}
-    # req = requests.get(url, json=data, headers=headers)
-    # io = StringIO(req.text)
-    # bins = json.load(io);
-    # binHeaders = {
-    # 'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
-    # }
-    # async with aiohttp.ClientSession(headers=binHeaders) as session:
 
-    #     for _bin in bins:
-    #             binUrl = 'https://api.jsonbin.io/v3/b/{}'.format(_bin["record"])
-    #             async with session.get(url=binUrl) as resp:
-    #                 bin = await resp.json()
-    #                 with open('results.json', 'w') as f:
-    #                     json.dump(bin, f)
-    #                 print(bin)
-                # binData = {}
-                # binReq = await requests.get(binUrl, json=binData, headers=binHeaders).json();
+# url = 'https://api.jsonbin.io/v3/c/uncategorized/bins'
+# headers = {
+# 'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
+# }
+# data = {}
+# req = requests.get(url, json=data, headers=headers)
+# io = StringIO(req.text)
+# bins = json.load(io);
+# binHeaders = {
+# 'X-Master-Key': '$2b$10$OpqpX1ClKRaQhj8QhbE/q.GYv2TZsb.RMyu1c8qIuye9Ki5scgBue'
+# }
+# async with aiohttp.ClientSession(headers=binHeaders) as session:
 
-            # with open('results.json', 'w') as f:
-            #         json.dump(binReq, f)
-                #df = pd.read_json(binReq.text);
-                #print(json.load(StringIO(binReq.text)))
-            #print(json.load(io))
+#     for _bin in bins:
+#             binUrl = 'https://api.jsonbin.io/v3/b/{}'.format(_bin["record"])
+#             async with session.get(url=binUrl) as resp:
+#                 bin = await resp.json()
+#                 with open('results.json', 'w') as f:
+#                     json.dump(bin, f)
+#                 print(bin)
+# binData = {}
+# binReq = await requests.get(binUrl, json=binData, headers=binHeaders).json();
 
-#asyncio.run(main())
+# with open('results.json', 'w') as f:
+#         json.dump(binReq, f)
+# df = pd.read_json(binReq.text);
+# print(json.load(StringIO(binReq.text)))
+# print(json.load(io))
+
+# asyncio.run(main())
